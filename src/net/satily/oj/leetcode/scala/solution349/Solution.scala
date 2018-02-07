80c5babdf085a532c35c81712de3abf5ef20900d
satily <houjiaxu@xiaomi.com>
2018-02-07 15:49:50 +0800

    solution349

---
+++ src/net/satily/oj/leetcode/scala/solution349/Solution.scala
@@ -0,0 +1,14 @@
+package net.satily.oj.leetcode.scala.solution349
+
+object Solution {
+    def intersection(nums1: Array[Int], nums2: Array[Int]): Array[Int] = {
+        val num2Set = nums2.toSet
+        nums1.filter(num2Set.contains).distinct
+    }
+}
+
+object Main {
+    def main(args: Array[String]): Unit = {
+        Solution.intersection(Array(1, 2, 2, 1), Array(2, 2)).foreach(println)
+    }
+}
