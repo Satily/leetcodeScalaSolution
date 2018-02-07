e38e948f6ef576263513804f6191ece6f2209d72
satily <houjiaxu@xiaomi.com>
2018-02-07 18:08:56 +0800

    solution657

---
+++ src/net/satily/oj/leetcode/scala/solution657/Solution.scala
@@ -0,0 +1,23 @@
+package net.satily.oj.leetcode.scala.solution657
+
+object Solution {
+    def judgeCircle(moves: String): Boolean = {
+        val direction = Map(
+            'U' -> (-1,  0),
+            'D' -> ( 1,  0),
+            'L' -> ( 0, -1),
+            'R' -> ( 0,  1)
+        )
+        moves.map(direction.get).map(_.get).fold((0, 0))((a, b) => (a._1 + b._1, a._2 + b._2)) match {
+            case (0 , 0) => true
+            case _ => false
+        }
+    }
+}
+
+object Main {
+    def main(args: Array[String]): Unit = {
+        println(Solution.judgeCircle(""))
+        println(Solution.judgeCircle("LL"))
+    }
+}
