13858be7c6da625dd0238999856e6ccec01a5ccf
satily <houjiaxu@xiaomi.com>
2018-03-30 21:19:07 +0800

    solution344

---
+++ src/net/satily/oj/leetcode/scala/solution344/Solution.scala
@@ -0,0 +1,11 @@
+package net.satily.oj.leetcode.scala.solution344
+
+object Solution {
+    def reverseString(s: String): String = s.reverse
+}
+
+object Main {
+    def main(args: Array[String]): Unit = {
+        println(Solution.reverseString("hello"))
+    }
+}
